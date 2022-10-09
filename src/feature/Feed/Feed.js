c744ac51fe38c05a61cdf3c3340c88cddc3a0c10
Cal <calpnicklin@gmail.com>
2022-10-09 11:03:15 +0100

    cleans up with ternary operator

--- src/feature/Feed/Feed.js
+++ src/feature/Feed/Feed.js
@@ -21,52 +21,35 @@ const Feed = () => {
     const { flair, chooseFlair } = useFlair();
 
     const filteredResponse = response.filter(post => post.data.link_flair_text === flair);
-    console.log(filteredResponse);
 
     useEffect(() => {
         chooseFlair('')
     }, [subreddit]);
 
-    if (flair === '') {
-        return (
-            <div className='feed'>
-                <main>
-                    {response && !isError && (response.map((post, index) => (
-                        <div className='post' key={index}>
-                            <Post
-                                post={post.data}
-                            />
-                        </div>
-                    )))}
-                    {isError && <div className="post">Something went wrong</div>}
-                </main>
-                <div className="flairContainer">
-                    {uniqueFlairs.map((flair, index) => (
-                        <Flair flair={flair} key={index} />
-                    ))}
-                </div>
+    return (
+        <div className='feed'>
+            <main>
+                {response && !isError && (
+                    (filteredResponse ? filteredResponse : response).map((post, index) => (
+                    <div className='post' key={index}>
+                        <Post
+                            post={post.data}
+                        />
+                    </div>
+                ))
+                )}
+            {isError && <div className="post">Something went wrong</div>}
+            </main>
+
+            {filteredResponse && (
+            <div className="flairContainer">
+                {uniqueFlairs.map((flair, index) => (
+                    <Flair flair={flair} key={index} />
+                ))}
             </div>
-        )
-    } else
-        return (
-            <div className='feed'>
-                <main>
-                    {response && !isError && (filteredResponse.map((post, index) => (
-                        <div className='post' key={index}>
-                            <Post
-                                post={post.data}
-                            />
-                        </div>
-                    )))}
-                    {isError && <div className="post">Something went wrong</div>}
-                </main>
-                <div className="flairContainer">
-                    {uniqueFlairs.map((flair, index) => (
-                        <Flair flair={flair} key={index} />
-                    ))}
-                </div>
-            </div>
-        )
+            )}
+        </div>
+    )
 };
 
 export default Feed;
